5e3a90083999dc4035cfd3ee1cb9f3e4ec735071
Christopher Yeninas <phantom@sundergaming.com>
2021-06-17 03:41:59 -0400

    Update NebulaPlugin.cs
    use PluginInfo.cs for BepInEx details so that they can be accessed from elsewhere and use HarmonyX's CreateAndPatchAll method for patching

--- NebulaPatcher/NebulaPlugin.cs
+++ NebulaPatcher/NebulaPlugin.cs
@@ -5,16 +5,15 @@ using NebulaPatcher.Logger;
 using NebulaPatcher.MonoBehaviours;
 using System;
 using System.IO;
+using System.Reflection;
 using UnityEngine;
 
 namespace NebulaPatcher
 {
-    [BepInPlugin("com.github.hubertgendron.nebula", "Nebula - Multiplayer Mod", "0.0.2.0")]
+    [BepInPlugin(PluginInfo.PLUGIN_ID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     [BepInProcess("DSPGAME.exe")]
     public class NebulaPlugin : BaseUnityPlugin
     {
-        Harmony harmony = new Harmony("com.github.hubertgendron.nebula");
-
         void Awake()
         {
             Log.Init(new BepInExLogger(Logger));
@@ -44,30 +43,23 @@ namespace NebulaPatcher
 
             try
             {
-                var assemblies = AppDomain.CurrentDomain.GetAssemblies();
-                foreach (var assembly in assemblies)
+                Log.Info($"Applying patches from assembly: {Assembly.GetExecutingAssembly().FullName}");
+#if DEBUG
+                if (Directory.Exists("./mmdump"))
                 {
-                    if (assembly.FullName.StartsWith("NebulaPatcher"))
+                    foreach (FileInfo file in new DirectoryInfo("./mmdump").GetFiles())
                     {
-                        Log.Info($"Applying patches from assembly: {assembly.FullName}");
-#if DEBUG
-                        if (Directory.Exists("./mmdump"))
-                        {
-                            foreach (FileInfo file in new DirectoryInfo("./mmdump").GetFiles())
-                            {
-                                file.Delete();
-                            }
-
-                            Environment.SetEnvironmentVariable("MONOMOD_DMD_TYPE", "cecil");
-                            Environment.SetEnvironmentVariable("MONOMOD_DMD_DUMP", "./mmdump");
-                        }
-#endif
-                        harmony.PatchAll(assembly);
-#if DEBUG
-                        Environment.SetEnvironmentVariable("MONOMOD_DMD_DUMP", "");
-#endif
+                        file.Delete();
                     }
+
+                    Environment.SetEnvironmentVariable("MONOMOD_DMD_TYPE", "cecil");
+                    Environment.SetEnvironmentVariable("MONOMOD_DMD_DUMP", "./mmdump");
                 }
+#endif
+                Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), PluginInfo.PLUGIN_ID);
+#if DEBUG
+                Environment.SetEnvironmentVariable("MONOMOD_DMD_DUMP", "");
+#endif
 
                 Log.Info("Patching completed successfully");
             }
