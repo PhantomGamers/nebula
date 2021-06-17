f656b744a1523d4be0defbb68b139127417f8e8b
Christopher Yeninas <phantom@sundergaming.com>
2021-06-17 03:16:47 -0400

    consolidate common buildtool patches

---
+++ NebulaPatcher/Patches/Dynamic/BuildTool_Common_Patch.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using NebulaWorld.Factory;
+
+namespace NebulaPatcher.Patches.Dynamic
+{
+    [HarmonyPatch]
+    class BuildTool_Common_Patch
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(BuildTool_Click), nameof(BuildTool_Click.CreatePrebuilds))]
+        [HarmonyPatch(typeof(BuildTool_Path), nameof(BuildTool_Path.CreatePrebuilds))]
+        [HarmonyPatch(typeof(BuildTool_Inserter), nameof(BuildTool_Inserter.CreatePrebuilds))]
+        public static bool CreatePrebuilds_Prefix(BuildTool __instance)
+        {
+            return BuildToolManager.CreatePrebuilds(__instance);
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(BuildTool_Click), nameof(BuildTool_Click.CheckBuildConditions))]
+        [HarmonyPatch(typeof(BuildTool_Path), nameof(BuildTool_Path.CheckBuildConditions))]
+        [HarmonyPatch(typeof(BuildTool_Inserter), nameof(BuildTool_Inserter.CheckBuildConditions))]
+        public static bool CheckBuildConditions(ref bool __result)
+        {
+            if (FactoryManager.IgnoreBasicBuildConditionChecks)
+            {
+                __result = true;
+                return false;
+            }
+            return true;
+        }
+    }
+}
--- NebulaPatcher/Patches/Dynamic/BuildTool_Path_Patch.cs
+++ NebulaPatcher/Patches/Dynamic/BuildTool_Path_Patch.cs
@@ -7,13 +7,6 @@ namespace NebulaPatcher.Patches.Dynamic
     [HarmonyPatch(typeof(BuildTool_Path))]
     class BuildTool_Path_Patch
     {
-        [HarmonyPrefix]
-        [HarmonyPatch("CreatePrebuilds")]
-        public static bool CreatePrebuilds_Prefix(BuildTool_Path __instance)
-        {
-            return BuildToolManager.CreatePrebuilds(__instance);
-        }
-
         [HarmonyPostfix]
         [HarmonyPatch("CreatePrebuilds")]
         public static void CreatePrebuilds_Postfix()
@@ -27,18 +20,6 @@ namespace NebulaPatcher.Patches.Dynamic
                 FactoryManager.IsFromClient = false;
                 FactoryManager.IsHumanInput = false;
             }
-        }
-
-        [HarmonyPrefix]
-        [HarmonyPatch("CheckBuildConditions")]
-        public static bool CheckBuildConditions(ref bool __result)
-        {
-            if (FactoryManager.IgnoreBasicBuildConditionChecks)
-            {
-                __result = true;
-                return false;
-            }
-            return true;
         }
     }
 }
