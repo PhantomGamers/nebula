f0cc341c567c658490879a4e8ff4b1166667931b
Christopher Yeninas <phantom@sundergaming.com>
2021-06-20 22:20:33 -0400

    Refactor item processing
    Patch Player.TryAddItemToPackage, Player.UseHandItems, and StorageComponent.TakeTailItems to only add or remove items for the requesting player. Should fix every prior instance of item duplication

--- NebulaPatcher/Patches/Dynamic/BuildTool_Common_Patch.cs
+++ NebulaPatcher/Patches/Dynamic/BuildTool_Common_Patch.cs
@@ -1,4 +1,6 @@
 ﻿using HarmonyLib;
+using NebulaModel.Packets.Factory;
+using NebulaWorld;
 using NebulaWorld.Factory;
 
 namespace NebulaPatcher.Patches.Dynamic
@@ -12,7 +14,29 @@ namespace NebulaPatcher.Patches.Dynamic
         [HarmonyPatch(typeof(BuildTool_Inserter), nameof(BuildTool_Inserter.CreatePrebuilds))]
         public static bool CreatePrebuilds_Prefix(BuildTool __instance)
         {
-            return BuildToolManager.CreatePrebuilds(__instance);
+            if (!SimulatedWorld.Initialized)
+                return true;
+
+            // Host will just broadcast event to other players
+            if (LocalPlayer.IsMasterClient)
+            {
+                if (!FactoryManager.IsFromClient)
+                {
+                    FactoryManager.IsHumanInput = true;
+                }
+                int planetId = FactoryManager.EventFactory?.planetId ?? GameMain.localPlanet?.id ?? -1;
+                LocalPlayer.SendPacketToStar(new CreatePrebuildsRequest(planetId, __instance.buildPreviews, FactoryManager.PacketAuthor == -1 ? LocalPlayer.PlayerId : FactoryManager.PacketAuthor, __instance.GetType().ToString()), GameMain.galaxy.PlanetById(planetId).star.id);
+            }
+
+            //If client builds, he need to first send request to the host and wait for reply
+            if (!LocalPlayer.IsMasterClient && !FactoryManager.EventFromServer)
+            {
+                // set this so the Transpiler can actually differentiate between own and other requests (as the CreatePrebuilds() method is only run when EventFromServer = true)
+                FactoryManager.IsHumanInput = true;
+                LocalPlayer.SendPacket(new CreatePrebuildsRequest(GameMain.localPlanet?.id ?? -1, __instance.buildPreviews, FactoryManager.PacketAuthor == -1 ? LocalPlayer.PlayerId : FactoryManager.PacketAuthor, __instance.GetType().ToString()));
+                return false;
+            }
+            return true;
         }
 
         [HarmonyPrefix]
